cb91343f15b027f64379c1cb4f806bac0db2a7df
Mihai Enuica <mihai.enuica@gmail.com>
2022-03-10 20:27:12 +0200

    Rendering a second version page in React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+function moreInfo() {
+  return window.alert('For more info call 444-555-5689');
+}
+
 function App() {
   return (
     <div>
-      {console.log('Test Successfully', 25)}
+      <div>
+        <h1>Landing Page Title</h1>
+      </div>
+      <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Inventore necessitatibus et rerum ab vel, om</p>
+      </div>
+      <br />
+      <div>
+        <button onClick={moreInfo}>Click Here for More Info</button>
+      </div>
+      <br />
+      <div>
+        <img src="https://www.catsforafrica.co.za/wp-content/uploads/2017/05/CofA-BFCat-Walking-Square-400x400.jpg" alt="Cats" />
+      </div>
+        
     </div>);
 }
 

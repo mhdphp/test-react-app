b25e040be3aefaa1a2426144df3847b43c321cc5
Mihai Enuica <mihai.enuica@gmail.com>
2022-03-11 11:32:43 +0200

    React Inline Styling

--- src/App.js
+++ src/App.js
@@ -7,21 +7,34 @@ function moreInfo() {
 
 function App() {
   return (
-    <div>
-      <div>
-        <h1>Landing Page Title</h1>
-      </div>
-      <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Inventore necessitatibus et rerum ab vel, om</p>
-      </div>
-      <br />
-      <div>
-        <button onClick={moreInfo}>Click Here for More Info</button>
-      </div>
-      <br />
-      <div>
-        <img src="https://www.catsforafrica.co.za/wp-content/uploads/2017/05/CofA-BFCat-Walking-Square-400x400.jpg" alt="Cats" />
+    <div style={{padding: '75px',
+                textAlign: 'center', 
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor:'yellowgreen'}}>
+      
+      <div style={{width: '75%', 
+                  backgroundColor: '#e6e6ff', 
+                  padding: '50px'}}>
+        <div>
+          <h1>Landing Page Title</h1>
+        </div>
+        <div>
+          <img 
+            src="https://www.catsforafrica.co.za/wp-content/uploads/2017/05/CofA-BFCat-Walking-Square-400x400.jpg" 
+            alt="Cats"
+            style={{width:'500px'}} />
+        </div>
+        <div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Inventore necessitatibus et rerum ab vel, om</p>
+        </div>
+          <br />
+        <div>
+          <button onClick={moreInfo}>Click Here for More Info</button>
+        </div>
+          <br />
       </div>
         
     </div>);
